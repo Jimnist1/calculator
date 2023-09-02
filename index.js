761ca8cdbb0970e5bdf995f2a6994f71c7dfd648
James Wimmers <jimmyskydive@gmail.com>
2023-09-02 21:06:46 +1200

    started with check status function to determine when functions should activate

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let inputArr1 = [];
-let inputArr2 = [];
+let currentArr = [];
+let nextArr2 = [];
 let currentTotal = 0;
 let nextTotal = 0;
-let sum1 = 0;
-let sum2 = 0;
+let currentSum = 0;
+let nextSum = 0;
 let operator = "";
 
-functio;
+//function errorHandler(button){};
+function create()
+
+function checkStatus(button) {
+  if (
+    (currentTotal == 0 && button.id == 1) ||
+    2 ||
+    3 ||
+    4 ||
+    5 ||
+    6 ||
+    7 ||
+    8 ||
+    9
+  ) {
+  }
+}
 
 function displayCurrent(total) {
   let inputCurrent = document.getElementById("inputContainer");
@@ -16,8 +32,7 @@ function displayCurrent(total) {
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    checkStatus();
-    displayCurrent(currentTotal);
+    checkStatus(button.id);
   });
 });
 const body = document.querySelector("body");
