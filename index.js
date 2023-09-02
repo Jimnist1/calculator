8980214f766ad707e11113306fa7ae849164caf9
James Wimmers <jimmyskydive@gmail.com>
2023-09-02 22:59:21 +1200

    added a check to see if zero should be added, to the array, and a switch to change which array a number is added to, another function has been added to delete a digit from the array

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
 let currentArr = [];
-let nextArr2 = [];
-let currentTotal = 0;
-let nextTotal = 0;
+let nextArr = [];
+let arraySet = currentArr;
+
 let currentSum = 0;
 let nextSum = 0;
+
+let currentTotal = 0;
+let nextTotal = 0;
+
 let operator = "";
 
-//function errorHandler(button){};
-function create()
+function arrayToSum(array) {
+  currentSum = array.join("");
+  currentTotal = currentSum;
+  displayCurrent(currentTotal);
+}
 
-function checkStatus(button) {
-  if (
-    (currentTotal == 0 && button.id == 1) ||
-    2 ||
-    3 ||
-    4 ||
-    5 ||
-    6 ||
-    7 ||
-    8 ||
-    9
-  ) {
+function deleteDigit(array) {
+  if (array.length > 1) {
+    array.pop();
+    arrayToSum(array);
+  } else {
+    array.pop();
+    array = [0];
+    arrayToSum(array);
+  }
+}
+
+function createSum(array, number) {
+  if (array.length <= 8) {
+    array.push(number);
+    console.log(array);
+    arrayToSum(array);
+  }
+}
+
+function checkZero(button) {
+  if (currentArr.length || nextArr2.length > 0) createSum(arraySet, button);
+}
+
+function changeArray() {
+  if (arraySet == currentArr) arraySet = nextArr;
+  else arraySet = currentArr;
+}
+
+function checkButton(button) {
+  switch (button) {
+    case "0":
+      checkZero(button);
+    case ".":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      createSum(arraySet, button);
+      break;
+    case "+":
+    case "-":
+    case "/":
+    case "*":
+      operator = button;
+
+      break;
+    case "delete":
+      deleteDigit(arraySet);
+      break;
+    case "clear":
+      break;
   }
 }
 
@@ -32,7 +83,7 @@ function displayCurrent(total) {
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    checkStatus(button.id);
+    checkButton(button.id);
   });
 });
 const body = document.querySelector("body");
